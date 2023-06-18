a54e0c0f3e7317d6787fbc5398d5a5bb5fedc926
Saad Tariq <tariqs26@mcmaster.ca>
2023-06-18 18:13:36 -0400

    remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Random Quote Generator",
@@ -15,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <main className="mx-auto p-12 bg-black min-h-screen text-white">
           {children}
         </main>
